ba9985c567142763573566a5b26ecb6f4fe692c3
vukojarob96 <37276972+vukojarob96@users.noreply.github.com>
2020-01-25 23:25:32 +0100

    Add files via upload

--- restservice.js
+++ restservice.js
@@ -29,6 +29,8 @@ app.post('/login',(req,res)=>{
 	
 	const pool = new Pool({connectionString: connectionString});
 	
+	
+	
 
     pool.connect(err =>{
 		
@@ -37,25 +39,25 @@ app.post('/login',(req,res)=>{
 			return;
 		};
 		
-		let query = 'SELECT * from public."Users" where email=$1';
-		let param = [req.body.email];
+		let query = 'SELECT * from public."Users" where email=$1 and pass=$2';
+		let param = [req.body.email, req.body.pass];
 		
 		
 		pool.query(query, param,(err,rs)=>{
         
 		if(err==null){
 			
-			let data = rs.rows;
-		
-			if((req.body.email==data[0].email||req.body.email==data[0].username)&&req.body.pass==data[0].pass){
-				
-				res.json("found");
-			}else{res.json("notfound")};
+			
+			
+			
+			
+			if(rs.rowCount==0)
+				res.json("notfound")
+			else
+				res.json("found")
 				
 			
 			
-			console.log(data);
-			data=null;
         
 		}
 		else{
@@ -93,25 +95,205 @@ app.post('/register',(req,res)=>{
 		
 		
 		
-		let query = 'INSERT INTO public."Users"(username,email,pass)VALUES ($1, $2, $3)';
-		let param = [req.body.user,req.body.email,req.body.pass];
+		let query = 'SELECT * from public."Users" where email=$1';
+		let param = [req.body.email];
+		let q = 'INSERT INTO public."Users"(username, email, pass)VALUES ($1,$2,$3);';
+		let p = [req.body.user, req.body.email,req.body.pass];
 		
-		pool.query(query,param,(err,rsp)=>{
+		pool.query(query,param).then((rsp)=>{
+			if(rsp.rowCount==0){
+				pool.query(q,p).then((result)=>{
+					res.json("registered");
+					pool.end();
+					
+				}).catch((err)=>{console.log(err)})
+			}
+			else{
+				res.json("aexists");
+				pool.end();
+			}
+				
 			
-			console.log(err,rsp);
-			res.json("registered");
-			
-			
-		});
+		}).catch((err)=>{console.log(err)});
 		
 		
 		
 		
-		pool.end();
+		
 	});
 
 
 });
+
+app.get('/profile',(req,res)=>{
+	
+	
+	const pool = new Pool({connectionString: connectionString}) ;
+	
+	//console.log(req.query.email);
+	
+	
+	
+	
+	pool.connect((err)=>{
+		
+		if(err)
+			cosole.log(err);
+		
+		
+		
+		let query = 'SELECT * from public."Users" where email=$1';
+		let param = [req.query.email];
+		
+		
+		pool.query(query,param,(err,rsp)=>{
+			
+			let data = rsp.rows;
+			console.log(data);
+			console.log(err,rsp);
+			res.json(data[0]);
+			
+			
+			
+		})
+		
+		pool.end();
+	});
+	
+	
+});
+
+app.route('/delete').delete((req,res)=>{
+	
+	
+	const pool = new Pool({connectionString: connectionString}) ;
+	
+	
+	console.log("start deleting");
+	console.log(req.query.email);
+	
+	pool.connect((err)=>{
+		
+		if(err)
+			cosole.log(err);
+		
+		
+		
+		let query = 'DELETE from public."Users" where email=$1';
+		let param = [req.query.email];
+		
+		
+		pool.query(query,param,(err,rsp)=>{
+			
+			if(err)
+				res.json("error");
+			
+			console.log(rsp);
+			console.log("deleted");
+			res.json("deleted");
+			
+			
+			
+		});
+		
+		pool.end();
+	});
+	
+	
+});
+
+app.put('/editpass',(req,res)=>{
+	
+	
+	const pool = new Pool({connectionString: connectionString}) ;
+	
+	
+	console.log("");
+	console.log(req.body.oldpass);
+	
+	
+	pool.connect((err)=>{
+		
+		if(err)
+			cosole.log(err);
+		
+		
+		
+		let query = 'SELECT * from public."Users" where email=$1 and pass=$2';
+		let param = [req.body.email, req.body.oldpass];
+		let q = 'UPDATE public."Users" SET pass=$2 WHERE email=$1';
+	
+		let p = [req.body.email, req.body.newpass];
+		
+		
+		pool.query(query,param).then((rsp)=>{
+			if(rsp.rowCount==0){
+			res.json("notfound");
+			
+			}else pool.query(q,p).then((result)=>{
+				console.log(result);
+				res.json("updated");
+				pool.end();
+			}).catch((err)=>{console.log(err)})
+			
+		}).catch((err)=>{console.log(err);});
+		
+		
+		
+		
+		
+		
+	});
+	
+	
+});
+
+
+app.put('/edituser',(req,res)=>{
+	
+	
+	const pool = new Pool({connectionString: connectionString}) ;
+	
+	
+	console.log("");
+	console.log(req.body.newuser);
+	
+	
+	pool.connect((err)=>{
+		
+		if(err)
+			cosole.log(err);
+		
+		
+		
+		
+		let query = 'UPDATE public."Users" SET username=$2 WHERE email=$1';
+	
+		let param = [req.body.email, req.body.newuser];
+		
+		
+		pool.query(query,param).then((rsp)=>{
+			if(rsp.rowCount>0){
+				res.send("updated");
+				pool.end();
+			}
+			else
+				pool.end();
+		
+			
+		}).catch((err)=>{console.log(err);});
+		
+		
+		
+		
+		
+		
+	});
+	
+	
+});
+
+
 
 //In the making: other routes
 	
